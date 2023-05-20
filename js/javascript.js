a1e0ae0b6b6c4058efafd9577c1f59b3cf323fdd
fnmarcos <127150588+fnmarcos@users.noreply.github.com>
2023-05-20 14:29:11 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+// Parte 1
+
+var nombre = "Marcos";
+var apellido = "Fernandez";
+
+console.log("Nombre y apellido: ",nombre + " " + apellido);
+
+
+// Parte 2
+
+
+var x = 5;
+var y = 2;
+
+var suma = x + y;
+
+console.log("Resultado primer suma: ",suma);
+
+var z = 2;
+var suma2 = suma + z;
+console.log("Resultado segunda suma: ",suma2);
